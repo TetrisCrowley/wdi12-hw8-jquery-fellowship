ac5b56f4e7a0e2aed2f2b43284356f16b375469c
Steve <sbrennison@gmail.com>
2018-06-25 08:59:18 -0500

    Updated Gandalf again

--- lotr_homework/js/app.js
+++ lotr_homework/js/app.js
@@ -259,12 +259,13 @@ const forgeTheFellowShip = () => {
 const theBalrog = () => {
 
   // 1. change the 'Gandalf' textNode to 'Gandalf the White'
-  $(`.buddy:contains('Gandalf')`).text('Gandalf the White')
+  $(`.buddy:contains('Gandalf')`).text('Gandalf the White');
 
   // 2. add a class "the-white" to this element
-
+  $(`.buddy:contains('Gandalf')`).attr('class', "the-white");
 
   // 3. in the style.css file, add a css rule to make elements of the class "the-white" have a white background and a grey border
+
 
 };
 
@@ -277,8 +278,10 @@ const theBalrog = () => {
 const hornOfGondor = () => {
 
   // 1. create a pop-up alert that the horn of gondor has been blown
+  alert.("Horn of Gondor has been blown!");
 
-  // 2. Boromir's been killed by the Uruk-hai! Put a linethrough on Boromir's name
+  // 2. Boromir's been killed by the Uruk-hai! Put a line through on Boromir's name
+
 
   // 3. Tricky: Remove the Uruk-Hai from the Baddies on the page
 
